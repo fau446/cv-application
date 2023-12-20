2b7c94d7f379216afe0f5e96d742a513243f8783
Franklin <Franklinau2@gmail.com>
2023-12-20 12:30:33 -0800

    Finish CV styling

--- src/components/CV/CV.jsx
+++ src/components/CV/CV.jsx
@@ -1,37 +1,52 @@
+import styles from "./CV.module.css";
+
 function CV({ generalInfo, educationInfo, practicalInfo }) {
   return (
-    <>
-      <div>
-        <h2>General Info</h2>
-        <p>{generalInfo.name}</p>
+    <div className={styles.cv}>
+      <div className={styles.generalSection}>
+        <p className={styles.name}>{generalInfo.name}</p>
         <p>{generalInfo.email}</p>
         <p>{generalInfo.phone}</p>
         <p>{generalInfo.address}</p>
       </div>
-      <div>
-        <h2>Education Info</h2>
+      <div className={styles.educationSection}>
+        <h2 className={styles.header}>Education</h2>
         {educationInfo.map((item) => (
-          <div key={item.uniqueId}>
-            <p>{item.schoolName}</p>
+          <div key={item.uniqueId} className={styles.entry}>
+            <div className={styles.top}>
+              <p className={styles.bold}>{item.schoolName}</p>
+              <div className={styles.date}>
+                <p className={styles.bold}>{item.startDate}</p>
+                {item.startDate && item.endDate && (
+                  <p className={styles.bold}>-</p>
+                )}
+                <p className={styles.bold}>{item.endDate}</p>
+              </div>
+            </div>
             <p>{item.degree}</p>
-            <p>{item.startDate}</p>
-            <p>{item.endDate}</p>
           </div>
         ))}
       </div>
-      <div>
-        <h2>Practical Info</h2>
+      <div className={styles.practicalSection}>
+        <h2 className={styles.header}>Work Experience</h2>
         {practicalInfo.map((item) => (
           <div key={item.uniqueId}>
-            <p>{item.company}</p>
+            <div className={styles.top}>
+              <p className={styles.bold}>{item.company}</p>
+              <div className={styles.date}>
+                <p className={styles.bold}>{item.startDate}</p>
+                {item.startDate && item.endDate && (
+                  <p className={styles.bold}>-</p>
+                )}
+                <p className={styles.bold}>{item.endDate}</p>
+              </div>
+            </div>
             <p>{item.position}</p>
-            <p>{item.startDate}</p>
-            <p>{item.endDate}</p>
             <p>{item.location}</p>
           </div>
         ))}
       </div>
-    </>
+    </div>
   );
 }
 
